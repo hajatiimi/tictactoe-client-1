d48ece15bd5b293c0f0ec77e041ee511bc6566ce
Tuomas Toivonen <toivotuo@kasvua.org>
2019-11-20 22:27:10 +0200

    Add server host and port as command line parameters

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tictactoe-client-1
+
+Usage: python3 client.py <server-host> <server-port>
--- client.py
+++ client.py
@@ -1,15 +1,16 @@
+#!/usr/bin/python3
 
 import socket
 
 class tictactoe_game:
   def __init__(self, gridsize):
     self.turn = 0
-    self.grid_size = gridsize 
+    self.grid_size = gridsize
     self.grid = create_grid(self.grid_size)
     self.current_player = 0
     self.won = False
     self.winner = 1
-  
+
   def take_turn(self, x, y):
     if self.grid[x][y] == 0:
       self.grid[x][y] = self.current_player + 1
@@ -20,7 +21,7 @@ class tictactoe_game:
       return True
     return False
 
-  
+
   def check_won(self, player):
     # Horizontal and vertical rows
     for x in range(self.grid_size):
@@ -39,8 +40,8 @@ class tictactoe_game:
     win1 = True
     win2 = True
     for k in range(self.grid_size):
-      win1 = win1 and self.grid[k][k] == player + 1 
-      win2 = win2 and self.grid[k][self.grid_size - k - 1] == player + 1 
+      win1 = win1 and self.grid[k][k] == player + 1
+      win2 = win2 and self.grid[k][self.grid_size - k - 1] == player + 1
       if win1 or win2:
         self.winner = player
         self.won = True
@@ -62,19 +63,19 @@ class tictactoe_game:
 
 def create_grid(n):
   grid = [[0 for x in range(n)] for y in range(n)]
-  return grid 
+  return grid
 
 
 
 def connect(host, port):
-  client_socket = socket.socket()  
-  client_socket.connect((host, port)) 
+  client_socket = socket.socket()
+  client_socket.connect((host, port))
   return client_socket
 
 
 
-def tictactoe_client():
-  client_socket = connect("localhost", 5000)
+def tictactoe_client(host, port):
+  client_socket = connect(host, port)
 
   def send(message):
     msg = message + "\n"
@@ -85,7 +86,7 @@ def tictactoe_client():
 
   send("GAME-JOIN")
   response = receive()
-  
+
   player_id = ""
   if response:
     response = response.split()
@@ -112,7 +113,14 @@ def tictactoe_client():
   # client_socket.close()  # close the connection
 
 
-  
+
 if __name__ == '__main__':
-  tictactoe_client()
-  
+  import sys
+
+  if len(sys.argv) != 3:
+    print("Usage: {} <server-host> <server-port>".format(sys.argv[0]))
+    sys.exit(1)
+
+  host, port = sys.argv[1], int(sys.argv[2])
+
+  tictactoe_client(host, port)
